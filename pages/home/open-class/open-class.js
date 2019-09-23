8c8d05d525d2821d15ed2d3ceebc49d82994795d
smilv <owou@icloud.com>
2019-09-23 15:57:53 +0800

    公开课

--- pages/home/open-class/open-class.js
+++ pages/home/open-class/open-class.js
@@ -1,24 +1,43 @@
-//bin 2019/9/23
-// pages/home/open-class/open-class.js
+/**
+ * bin 2019/9/23
+ * 公开课
+ */
+
+const request = require("../../../request/index.js")
+const uri = require("../../../uri/index.js")
+
 Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        data: null
+    },
+
 	/**
-	 * 组件的属性列表
+	 * 组件的生命周期
 	 */
-	properties: {
+    lifetimes: {
+        attached: function() {
+            request.openClass(133).then(res => {
+                this.setData({
+                    data: res.data
+                })
+            })
+        }
+    },
 
-	},
+    /**
+     * 组件的方法列表
+     */
+    methods: {
 
-	/**
-	 * 组件的初始数据
-	 */
-	data: {
-
-	},
-
-	/**
-	 * 组件的方法列表
-	 */
-	methods: {
-
-	}
+    }
 })
--- request/index.js
+++ request/index.js
@@ -19,15 +19,22 @@ const request = (url, method, header, data) => {
 }
 
 module.exports = {
+    //首页广告
     recommend: () => {
         return request(uri.skuPath + '/wap/recommend')
     },
+    //sku配置
     skuConfig: data => {
         return request(uri.skuPath + '/index/config/sku', 'POST', {
             'content-type': 'application/x-www-form-urlencoded'
         }, data)
     },
+    //首页banner
     banner: skuId => {
         return request(uri.skuPath + '/wap/carousel/' + skuId)
+    },
+    //公开课列表
+    openClass: skuId => {
+        return request(uri.skuPath + '/wap/openLives/' + skuId)
     }
 }
